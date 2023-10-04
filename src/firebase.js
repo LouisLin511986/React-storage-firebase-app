8c5c5563cfc6a5abfff23a21356386b67378e6d8
Louis511986 <90818126@gcloud.csu.edu.tw>
2023-10-04 16:53:15 +0800

    1653

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxz04z-v-u3B4EWzmzcCItreo2WkGYhR8",
-    authDomain: "storage-firebase-app-a3e53.firebaseapp.com",
-    projectId: "storage-firebase-app-a3e53",
-    storageBucket: "storage-firebase-app-a3e53.appspot.com",
-    messagingSenderId: "541378025253",
-    appId: "1:541378025253:web:1ae798a8ab4414f4f1eccc"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
